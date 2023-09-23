f7875ceb5eeb1abf3b6e07cb98dd400f3ea38c1b
sheriffdeen-yusuf <Sheriffdeenyusuf1130@gmail.com>
2023-09-23 05:05:57 +0000

    added search feature

---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Loader() {
+  return (
+    <div className="mx-auto p-8 rounded-full h-6 w-6 border-2 border-slate-400/80 border-t-0 animate-spin"></div>
+  );
+}
+
+export default Loader;
--- src/pages/gallery/index.tsx
+++ src/pages/gallery/index.tsx
@@ -4,6 +4,7 @@ import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { signOut } from "firebase/auth";
 import { auth } from "@/pages/api/firebase";
 import { useRouter } from "next/router";
+import Loader from "@/components/Loader";
 
 const imageTags = {
   1: "fashion",
@@ -34,6 +35,7 @@ function Index() {
 
   const [searchQuery, setSearchQuery] = useState("");
   const [filteredItems, setFilteredItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleSearch = (query) => {
     const filtered = Object.keys(imageTags).filter((key) =>
@@ -59,14 +61,19 @@ function Index() {
 
   // Apply filtering when the search query changes
   useEffect(() => {
+    setIsLoading(true);
+    const timerId = setTimeout(() => {
+      setIsLoading(false);
+    }, 2500);
     handleSearch(searchQuery);
+    return () => clearTimeout(timerId);
   }, [searchQuery]);
 
   return (
     <DragDropContext onDragEnd={draggableHandle}>
       <div className="relative">
         {/* ... Rest of your code ... */}
-        <header className="z-30 bg-stone-100 p-4 w-full fixed top-0 left-0 flex justify-around">
+        <header className="backdrop-blur-sm z-30 bg-white/80 p-4 w-full fixed top-0 left-0 flex justify-around">
           <h1>Draggize</h1>
           <button
             onClick={handleLogout}
@@ -88,20 +95,6 @@ function Index() {
               onChange={(e) => setSearchQuery(e.target.value)}
               className="w-[250px] md:w-[600px] bg-slate-200 rounded-full px-4 py-2.5 focus:outline-none focus:ring focus:ring-amber-600 focus:ring-offset-4 "
             />
-            {/* <div className="flex flex-row mt-4 justify-center gap-2">
-              <div className="px-1 md:px-6 py-1 rounded-md bg-green-200 text-green-500">
-                fashion
-              </div>
-              <div className="px-1 md:px-6 py-1 rounded-md bg-yellow-200 text-yellow-500">
-                outdoor
-              </div>
-              <div className="px-1 md:px-6 py-1 rounded-md bg-purple-200 text-purple-500">
-                nature
-              </div>
-              <div className="px-1 md:px-6 py-1 rounded-md bg-sky-200 text-sky-500">
-                cars
-              </div>
-            </div> */}
           </div>
         </div>
 
@@ -113,31 +106,39 @@ function Index() {
               ref={provided.innerRef}
               className="bg-red-100 px-20 py-8 w-full"
             >
-              <div className="grid gap-6 grid-cols-1 flex-grow md:grid-cols-3">
-                {filteredItems.map((idx, i) => (
-                  <Draggable key={i} draggableId={`${i + 1}`} index={i}>
-                    {(provided) => (
-                      <div className="p-2  bg-green-400/80">
-                        <Image
-                          alt={`image${idx}`}
-                          src={`/${idx}.jpeg`}
-                          height={500}
-                          width={400}
-                          ref={provided.innerRef}
-                          {...provided.dragHandleProps}
-                          {...provided.draggableProps}
-                          className="max-h-[33rem]"
-                        />
-                        <div className="mt-2">
-                          <p>
-                            tag: <span>{imageTags[idx]}</span>
-                          </p>
+              {isLoading && <Loader />}
+              {filteredItems.length === 0 && !isLoading && (
+                <h3 className="text-center text-2xl font-medium">
+                  no search found
+                </h3>
+              )}
+              {!isLoading && (
+                <div className="grid gap-6 grid-cols-1 flex-grow md:grid-cols-3">
+                  {filteredItems.map((idx, i) => (
+                    <Draggable key={i} draggableId={`${i + 1}`} index={i}>
+                      {(provided) => (
+                        <div className="p-2  bg-green-200/80">
+                          <Image
+                            alt={`image${idx}`}
+                            src={`/${idx}.jpeg`}
+                            height={500}
+                            width={400}
+                            ref={provided.innerRef}
+                            {...provided.dragHandleProps}
+                            {...provided.draggableProps}
+                            className="max-h-[33rem]"
+                          />
+                          <div className="mt-2">
+                            <p>
+                              tag: <span>{imageTags[idx]}</span>
+                            </p>
+                          </div>
                         </div>
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-              </div>
+                      )}
+                    </Draggable>
+                  ))}
+                </div>
+              )}
             </main>
           )}
         </Droppable>
